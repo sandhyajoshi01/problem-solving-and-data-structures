e5a597dec39ff122eab065fb252e6370543c1535
sandhyajoshi01 <51840646+sandhyajoshi01@users.noreply.github.com>
2021-10-11 23:42:48 -0700

    Delete learningBasicsOfJS directory

--- learningBasicsOfJS/indexInString.js
+++
@@ -1,6 +0,0 @@
-const num = 20;
-
-const  ind =num.toString().indexOf(0)
-console.log(ind)
-console.log(num.toString().length)
-
--- learningBasicsOfJS/mapAndSet.js
+++
@@ -1,2 +0,0 @@
-const mapEg = new Map([[1,'name'],[2,'age']])
-console.log(mapEg.get(1))
